c7f91251dcdcffeccfa4d466b3c80492f975bd88
Mason199004 <mason1990haines@gmail.com>
2023-05-25 03:54:58 -0500

    new

--- taproot/taproot.c
+++ taproot/taproot.c
@@ -1,2 +1,68 @@
+#include <stdlib.h>
+#include <mem.h>
 #include "taproot.h"
 
+
+TapRoot_ThreadGlobal* TapRoot_InitThreadGlobal(TapRoot_Id id)
+{
+	TapRoot_ThreadGlobal* thgl = malloc(sizeof(TapRoot_ThreadGlobal));
+	memset(thgl, 0, sizeof(TapRoot_ThreadGlobal));
+	thgl->Self = id;
+	TapRoot_ResizeBuffer(thgl, AddressableThreads, 8);
+	TapRoot_ResizeBuffer(thgl, EventQueues, 8);
+	return thgl;
+}
+
+int TapRoot_CreateThread(TapRoot_Id NewThreadId, TapRoot_ThreadGlobal inherit, void* (*func)(void* threadGlobal) )
+{
+	TapRoot_ThreadGlobal* thgl = malloc(sizeof(TapRoot_ThreadGlobal));
+	*thgl = inherit;
+	thgl->Self = NewThreadId;
+
+	for (int i = 0; i < thgl->AddressableThreadCount; ++i)
+	{
+		if (thgl->AddressableThreads[i] == TapRoot_Invalid) continue;
+
+		TapRoot_InsertNewThread(thgl->AddressableThreadGlobals[i], thgl);
+	}
+
+	pthread_t p;
+	int thread = pthread_create(&p, NULL, func, thgl);
+	if (thread) //not good
+	{
+		TapRoot_DestroySelf(thgl); /* not efficient error handling but can prevent issues caused by the target thread spawning immediately
+		                                  and not being addressable or receivable by other threads */
+		return -1;
+	}
+
+	return 0;
+}
+
+TapRoot_Event TapRoot_IntoEvent(uint32_t type, void* data)
+{
+	TapRoot_Event event;
+	event.EventType = type;
+	event.EventData = data;
+	return event;
+}
+
+void TapRoot_PushEvent(TapRoot_Event event, TapRoot_Id destination, TapRoot_ThreadGlobal* global)
+{
+	
+}
+
+void TapRoot_GetLock(TapRoot_EventQueue* queue);
+
+int TapRoot_MaybeGetLock(TapRoot_EventQueue* queue);
+
+void TapRoot_Unlock(TapRoot_EventQueue* queue);
+
+void TapRoot_ClearQueue(TapRoot_EventQueue* queue);
+
+int32_t TapRoot_CreateWorker(TapRoot_ThreadGlobal* self, void* (*func)(void* threadGlobal));
+
+void TapRoot_DestroySelf(TapRoot_ThreadGlobal* self);
+
+void TapRoot_ResizeBuffer(TapRoot_ThreadGlobal* global, TapRoot_ResizableBuffer bufferType, uint64_t newSize);
+
+void TapRoot_InsertNewThread(TapRoot_ThreadGlobal* target, TapRoot_ThreadGlobal* toInsert);
--- taproot/taproot.h
+++ taproot/taproot.h
@@ -19,16 +19,23 @@ typedef struct
 	uint32_t EventCount;
 } TapRoot_EventQueue;
 
-typedef struct
+typedef struct TapRoot_ThreadGlobal
 {
-	TapRoot_Id Self;
-	TapRoot_Id* AddressableThreads;
-	TapRoot_EventQueue* ThreadQueues;
-	uint64_t AddressableThreadCount;
+       TapRoot_Id Self;
+       TapRoot_Id* AddressableThreads;
+struct TapRoot_ThreadGlobal** AddressableThreadGlobals;
+       TapRoot_EventQueue* ThreadQueues;
+       uint64_t AddressableThreadCount;
+       uint64_t EventQueueCount;
+	   uint64_t __AddrThAlloc;
+	   uint64_t __EvQuAlloc;
 } TapRoot_ThreadGlobal;
+
+#define TapRoot_Invalid UINT64_MAX
 
 TapRoot_ThreadGlobal* TapRoot_InitThreadGlobal(TapRoot_Id id);
 
+int TapRoot_CreateThread(TapRoot_Id NewThreadId, TapRoot_ThreadGlobal inherit, void* (*func)(void* threadGlobal) );
 
 TapRoot_Event TapRoot_IntoEvent(uint32_t type, void* data);
 
@@ -42,8 +49,20 @@ void TapRoot_Unlock(TapRoot_EventQueue* queue);
 
 void TapRoot_ClearQueue(TapRoot_EventQueue* queue);
 
-int32_t TapRoot_CreateWorker(TapRoot_ThreadGlobal* self, void(TapRoot_ThreadGlobal*, uint32_t, void*));
+int32_t TapRoot_CreateWorker(TapRoot_ThreadGlobal* self, void* (*func)(void* threadGlobal));
 
 void TapRoot_DestroySelf(TapRoot_ThreadGlobal* self);
+
+typedef enum
+{
+	AddressableThreads,
+	EventQueues
+} TapRoot_ResizableBuffer;
+
+void TapRoot_ResizeBuffer(TapRoot_ThreadGlobal* global, TapRoot_ResizableBuffer bufferType, uint64_t newSize);
+
+void TapRoot_InsertNewThread(TapRoot_ThreadGlobal* target, TapRoot_ThreadGlobal* toInsert);
+
+
 
 
